7585ec800fac4de3c74d57249bc23cae14f6cded
yagnesh <yagnesh.bhat@gmail.com>
2019-08-17 11:23:46 -0400

    Ch02 Exercises solved

---
+++ exercises/ch02/solution.py
@@ -0,0 +1,11 @@
+seconds_per_hour = 60 * 60
+print(seconds_per_hour)
+
+seconds_per_day = seconds_per_hour * 24
+print(seconds_per_day)
+
+div_float = seconds_per_day / seconds_per_hour
+print(div_float)
+
+div_int = seconds_per_day // seconds_per_hour
+print(div_int)
